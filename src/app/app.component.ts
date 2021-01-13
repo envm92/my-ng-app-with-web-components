d6c4f440111b889f6b89592e024ba5859604b3c5
Eri <envm92@gmail.com>
2021-01-13 12:03:09 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  myTitle = 'project-angular';
+  open = false;
+
+  controlsDemo0 = [
+    { label: 'Label', icon: 'favorite' },
+    { label: 'Label', img: 'https://via.placeholder.com/150' },
+    { label: 'Label', img: 'https://www.flaticon.com/svg/vstatic/svg/1828/1828108.svg?token=exp=1610405314~hmac=67ab7bd516c3212208bcaa356cf9faea' },
+    { label: 'Label' },
+    { label: 'Label' }];
+  controlsDemo1 = [
+    { label: 'Label' },
+    { label: 'Label' },
+    { label: 'Label' }];
+  controlsDemo2 = [
+    { label: 'Label' },
+    { label: 'Label' }];
+  controlsDemo3 = [
+    { label: 'Label', icon: 'image' },
+    { label: 'Label', icon: 'image' }
+  ];
 }
