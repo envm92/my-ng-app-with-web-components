5e7d1a8e9a115e3033bf34dec7de4f148dc71551
Eri <envm92@gmail.com>
2021-01-14 12:25:46 -0600

    Update packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   controlsDemo0 = [
     { label: 'Label', icon: 'favorite' },
     { label: 'Label', img: 'https://via.placeholder.com/150' },
-    { label: 'Label', img: 'https://www.flaticon.com/svg/vstatic/svg/1828/1828108.svg?token=exp=1610405314~hmac=67ab7bd516c3212208bcaa356cf9faea' },
+    { label: 'Label', img: 'https://svgur.com/i/T78.svg' },
     { label: 'Label' },
     { label: 'Label' }];
   controlsDemo1 = [
